f1c3b17443e84243c81ba0b4bfb6e9f0df13ea03
Dane Shenton <dane.shenton@gmail.com>
2019-04-07 21:37:27 +0200

    Added static files to express for setting up encryption

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import routes from './routes';
 
 console.log('We are in: ' + process.env.NODE_ENV);
 
+if (process.env.NODE_ENV === 'production') {
+
+}
+
 const app = express();
 const port = 3080;
+
+app.use(express.static(process.env.STATIC_FILES_PATH, { dotfiles: 'allow' } ))
 
 routes(app);
 
