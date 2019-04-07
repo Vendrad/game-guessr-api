2ae64a5c228f2ab405c1ce98a2f8b97f5049eac0
Dane Shenton <dane.shenton@gmail.com>
2019-04-07 22:26:18 +0200

    Added SSL listener.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import express from 'express';
+import fs from 'fs';
+import http from 'http';
 import https from 'https';
 import 'dotenv/config';
 import routes from './routes';
 
 console.log('We are in: ' + process.env.NODE_ENV);
 
-if (process.env.NODE_ENV === 'production') {
-
-}
-
 const app = express();
-const port = 3080;
+const port = process.env.PORT;
+const port_ssl = process.env.PORT_SSL;
 
 app.use(express.static(process.env.STATIC_FILES_PATH, { dotfiles: 'allow' } ))
 
 routes(app);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+http.createServer(app).listen(port, () => {
+  console.log(`Listening on port ${port}.`)
+});
+
+if (process.env.SSL_PATH_TO_KEY !== '') {
+  https.createServer({
+    key: fs.readFileSync(process.env.SSL_PATH_TO_KEY),
+    cert: fs.readFileSync(process.env.SSL_PATH_TO_CERT),
+    ca: fs.readFileSync(process.env.SSL_PATH_TO_CHAIN)
+  }, app).listen(port_ssl, () => {
+    console.log(`Listening on SSL port ${port_ssl}.`)
+  });
+}
